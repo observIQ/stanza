a8f7f26e122a3c7905018cc866c913051e0dde03
Camden Cheek <camden@ccheek.com>
2020-02-19 14:44:38 -0500

    Remove unnecessary struct tags

--- config/config.go
+++ config/config.go
@@ -5,5 +5,5 @@ import "github.com/bluemedora/log-agent/plugin"
 type Config struct {
 	LogLevel string
 	LogPath  string
-	Plugins  []plugin.PluginConfig `mapstructure:"plugins"`
+	Plugins  []plugin.PluginConfig
 }
