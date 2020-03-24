9246fbf978f3e46dd1e82e15260bd51af3ed347a
Camden Cheek <camden@ccheek.com>
2020-03-24 13:49:04 -0400

    Reorganize unmarshall hooks
    Move top-level hook into the config package, but keep the plugin
    specific hook in the plugin package

--- plugin/config.go
+++ plugin/config.go
@@ -47,48 +47,42 @@ type BuildContext struct {
 	Logger   *zap.SugaredLogger
 }
 
-func UnmarshalHook(c *mapstructure.DecoderConfig) {
-	c.DecodeHook = newPluginConfigDecoder()
-}
-
-func newPluginConfigDecoder() mapstructure.DecodeHookFunc {
-	return func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
-		var m map[interface{}]interface{}
-		if f != reflect.TypeOf(m) {
-			return data, nil
-		}
-
-		if t.String() != "plugin.PluginConfig" {
-			return data, nil
-		}
-
-		d, ok := data.(map[interface{}]interface{})
-		if !ok {
-			return nil, fmt.Errorf("unexpected data type %T for plugin config", data)
-		}
-
-		typeInterface, ok := d["type"]
-		if !ok {
-			return nil, fmt.Errorf("missing type field for plugin config")
-		}
-
-		typeString, ok := typeInterface.(string)
-		if !ok {
-			return nil, fmt.Errorf("unexpected type %T for plugin config type", typeInterface)
-		}
-
-		configDefinitionFunc, ok := PluginConfigDefinitions[typeString]
-		if !ok {
-			return nil, fmt.Errorf("unknown plugin config type %s", typeString)
-		}
-
-		configDefinition := configDefinitionFunc()
-		// TODO handle unused keys
-		err := mapstructure.Decode(data, &configDefinition)
-		if err != nil {
-			return nil, fmt.Errorf("decode plugin definition: %s", err)
-		}
-
-		return configDefinition, nil
+var PluginConfigDecoder mapstructure.DecodeHookFunc = func(f reflect.Type, t reflect.Type, data interface{}) (interface{}, error) {
+	var m map[interface{}]interface{}
+	if f != reflect.TypeOf(m) {
+		return data, nil
 	}
+
+	if t.String() != "plugin.PluginConfig" {
+		return data, nil
+	}
+
+	d, ok := data.(map[interface{}]interface{})
+	if !ok {
+		return nil, fmt.Errorf("unexpected data type %T for plugin config", data)
+	}
+
+	typeInterface, ok := d["type"]
+	if !ok {
+		return nil, fmt.Errorf("missing type field for plugin config")
+	}
+
+	typeString, ok := typeInterface.(string)
+	if !ok {
+		return nil, fmt.Errorf("unexpected type %T for plugin config type", typeInterface)
+	}
+
+	configDefinitionFunc, ok := PluginConfigDefinitions[typeString]
+	if !ok {
+		return nil, fmt.Errorf("unknown plugin config type %s", typeString)
+	}
+
+	configDefinition := configDefinitionFunc()
+	// TODO handle unused keys
+	err := mapstructure.Decode(data, &configDefinition)
+	if err != nil {
+		return nil, fmt.Errorf("decode plugin definition: %s", err)
+	}
+
+	return configDefinition, nil
 }
