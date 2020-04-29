6019438436efa021ad61512c33112f8b5e0649ea
Camden Cheek <camden@ccheek.com>
2020-04-29 13:54:20 -0400

    Support alternative map types for op decoder
    Previously, we only supported map[interface{}]interface{}. This adds
    support for map[string]interface{} as it is generated by the json
    package. Additionally added a large number of tests for op decoding that
    should cover all the basic cases.

--- plugin/builtin/plugin_restructure_test.go
+++ plugin/builtin/plugin_restructure_test.go
@@ -4,11 +4,15 @@ import (
 	"testing"
 	"time"
 
+	"github.com/antonmedv/expr"
+	"github.com/antonmedv/expr/vm"
 	"github.com/bluemedora/bplogagent/entry"
 	"github.com/bluemedora/bplogagent/plugin/helper"
 	"github.com/bluemedora/bplogagent/plugin/testutil"
+	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
 
@@ -130,9 +134,201 @@ func TestRestructurePlugin(t *testing.T) {
 			}).Return(nil)
 
 			err := plugin.Process(tc.input)
-			if !assert.NoError(t, err) {
-				t.FailNow()
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestRestructureOpDecodeHook(t *testing.T) {
+	cases := []struct {
+		name             string
+		input            interface{}
+		expected         Op
+		errorRequirement require.ErrorAssertionFunc
+	}{
+		{
+			name: "OpAdd with value from map[string]interface{}",
+			input: map[string]interface{}{
+				"add": map[string]interface{}{
+					"field": "message",
+					"value": "newvalue",
+				},
+			},
+			expected: &OpAdd{
+				Field: []string{"message"},
+				Value: "newvalue",
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpAdd with value from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"add": map[interface{}]interface{}{
+					"field": "message",
+					"value": "newvalue",
+				},
+			},
+			expected: &OpAdd{
+				Field: []string{"message"},
+				Value: "newvalue",
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpAdd with value_expr from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"add": map[interface{}]interface{}{
+					"field":      "message",
+					"value_expr": `"newvalue"`,
+				},
+			},
+			expected: &OpAdd{
+				Field: []string{"message"},
+				ValueExpr: func() *vm.Program {
+					prog, err := expr.Compile(`"newvalue"`)
+					require.NoError(t, err)
+					return prog
+				}(),
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpRemove from map[string]interface{}",
+			input: map[string]interface{}{
+				"remove": "message",
+			},
+			expected: &OpRemove{
+				Field: []string{"message"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpRemove from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"remove": "message",
+			},
+			expected: &OpRemove{
+				Field: []string{"message"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpRetain from map[string]interface{}",
+			input: map[string]interface{}{
+				"retain": []string{"message"},
+			},
+			expected: &OpRetain{
+				Fields: []entry.FieldSelector{[]string{"message"}},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpRetain from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"retain": []string{"message"},
+			},
+			expected: &OpRetain{
+				Fields: []entry.FieldSelector{[]string{"message"}},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpRetain from map[string]interface{}",
+			input: map[string]interface{}{
+				"retain": []string{"message"},
+			},
+			expected: &OpRetain{
+				Fields: []entry.FieldSelector{[]string{"message"}},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpMove from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"move": map[interface{}]interface{}{
+					"from": "message",
+					"to":   "message2",
+				},
+			},
+			expected: &OpMove{
+				From: []string{"message"},
+				To:   []string{"message2"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpMove from map[string]interface{}",
+			input: map[string]interface{}{
+				"move": map[string]interface{}{
+					"from": "message",
+					"to":   "message2",
+				},
+			},
+			expected: &OpMove{
+				From: []string{"message"},
+				To:   []string{"message2"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpFlatten from map[string]interface{}",
+			input: map[string]interface{}{
+				"flatten": "message",
+			},
+			expected: &OpFlatten{
+				Field: []string{"message"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "OpFlatten from map[interface{}]interface{}",
+			input: map[interface{}]interface{}{
+				"flatten": "message",
+			},
+			expected: &OpFlatten{
+				Field: []string{"message"},
+			},
+			errorRequirement: require.NoError,
+		},
+		{
+			name: "Error invalid Op",
+			input: map[string]interface{}{
+				"invalid": "test",
+			},
+			errorRequirement: require.Error,
+		},
+		{
+			name:             "Error invalid type",
+			input:            42,
+			errorRequirement: require.Error,
+		},
+		{
+			name: "OpAdd error value and value_expr defined",
+			input: map[string]interface{}{
+				"add": map[string]interface{}{
+					"field":      "message",
+					"value":      "asdf",
+					"value_expr": `"asdf"`,
+				},
+			},
+			errorRequirement: require.Error,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var op Op
+			cfg := &mapstructure.DecoderConfig{
+				Result:     &op,
+				DecodeHook: OpDecoder,
 			}
+			decoder, err := mapstructure.NewDecoder(cfg)
+			require.NoError(t, err)
+
+			err = decoder.Decode(tc.input)
+			tc.errorRequirement(t, err)
+
+			require.Equal(t, op, tc.expected)
 		})
 	}
 }
