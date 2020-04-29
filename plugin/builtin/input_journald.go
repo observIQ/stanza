e3035738e53faf17fc33f6912bd1472440150afd
Camden Cheek <camden@ccheek.com>
2020-04-29 17:54:47 -0400

    Use journalctl binary rather than cgo bindings

--- plugin/builtin/input_journald.go
+++ plugin/builtin/input_journald.go
@@ -1,18 +1,23 @@
 // +build linux
-// +build cgo
 
 package builtin
 
 import (
+	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
 	"sync"
 	"time"
 
 	"github.com/bluemedora/bplogagent/entry"
 	"github.com/bluemedora/bplogagent/plugin"
 	"github.com/bluemedora/bplogagent/plugin/helper"
-	"github.com/coreos/go-systemd/sdjournal"
+	jsoniter "github.com/json-iterator/go"
 	"go.uber.org/zap"
 )
 
@@ -23,8 +28,8 @@ func init() {
 type JournaldInputConfig struct {
 	helper.BasicPluginConfig `mapstructure:",squash" yaml:",inline"`
 	helper.BasicInputConfig  `mapstructure:",squash" yaml:",inline"`
-	Directory                *string
-	Files                    []string
+	Directory                *string  `mapstructure:"directory"`
+	Files                    []string `mapstructure:"files"`
 }
 
 func (c JournaldInputConfig) Build(context plugin.BuildContext) (plugin.Plugin, error) {
@@ -38,23 +43,32 @@ func (c JournaldInputConfig) Build(context plugin.BuildContext) (plugin.Plugin, 
 		return nil, err
 	}
 
-	var journal *sdjournal.Journal
+	args := make([]string, 0, 10)
+
+	// Export logs in UTC time
+	args = append(args, "--utc")
+
+	// Export logs as JSON
+	args = append(args, "--output=json")
+
+	// Continue watching logs until cancelled
+	args = append(args, "--follow", "--no-tail")
+
 	switch {
 	case c.Directory != nil:
-		journal, err = sdjournal.NewJournalFromDir(*c.Directory)
+		args = append(args, "--directory", *c.Directory)
 	case len(c.Files) > 0:
-		journal, err = sdjournal.NewJournalFromFiles(c.Files...)
-	default:
-		journal, err = sdjournal.NewJournal()
-	}
-	if err != nil {
-		return nil, fmt.Errorf("create journal: %w", err)
+		for _, file := range c.Files {
+			args = append(args, "--file", file)
+		}
 	}
 
 	journaldInput := &JournaldInput{
 		BasicPlugin: basicPlugin,
 		BasicInput:  basicInput,
-		journal:     journal,
+		binary:      "journalctl",
+		args:        args,
+		json:        jsoniter.ConfigFastest,
 	}
 	return journaldInput, nil
 }
@@ -63,68 +77,114 @@ type JournaldInput struct {
 	helper.BasicPlugin
 	helper.BasicInput
 
-	journal *sdjournal.Journal
+	binary string
+	args   []string
 
+	json   jsoniter.API
 	cancel context.CancelFunc
 	wg     *sync.WaitGroup
 }
 
 // Start will start generating log entries.
-func (g *JournaldInput) Start() error {
+func (plugin *JournaldInput) Start() error {
 	// TODO protect against multiple starts?
 	ctx, cancel := context.WithCancel(context.Background())
-	g.cancel = cancel
-	g.wg = &sync.WaitGroup{}
+	plugin.cancel = cancel
+	plugin.wg = &sync.WaitGroup{}
 
-	g.wg.Add(1)
+	plugin.Debugw("Starting journald", "args", plugin.args)
+	cmd := exec.Command(plugin.binary, plugin.args...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("failed to get journalctl stdout: %s", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return fmt.Errorf("start journalctl: %s", err)
+	}
+
+	plugin.wg.Add(1)
 	go func() {
-		defer g.wg.Done()
+		defer plugin.wg.Done()
+		<-ctx.Done()
+		_ = cmd.Process.Signal(os.Interrupt)
+		_, _ = cmd.Process.Wait()
+	}()
+
+	plugin.wg.Add(1)
+	go func() {
+		defer plugin.wg.Done()
+
+		stdoutBuf := bufio.NewReader(stdout)
+
 		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
-
-			num, err := g.journal.Next()
+			line, err := stdoutBuf.ReadBytes('\n')
 			if err != nil {
-				g.Infow("Failed to advance journal cursor", zap.Error(err))
-				return
-			}
-
-			if num == 0 {
-				signal := make(chan struct{})
-				go func() {
-					g.journal.Wait(sdjournal.IndefiniteWait)
-					close(signal)
-				}()
-				select {
-				case <-signal:
-					continue
-				case <-ctx.Done():
-					return
+				if err != io.EOF {
+					plugin.Errorw("Received error reading from journalctl stdout", zap.Error(err))
 				}
+				return
 			}
 
-			jEntry, err := g.journal.GetEntry()
-			newRecord := make(map[string]interface{})
-			for k, v := range jEntry.Fields {
-				newRecord[k] = v
-			}
-			newEntry := entry.Entry{
-				Timestamp: time.Unix(0, int64(jEntry.RealtimeTimestamp)*1000), // from microseconds
-				Record:    newRecord,
-			}
-
-			err = g.Output.Process(&newEntry)
+			entry, _, err := plugin.parseJournalEntry(line) // TODO do something with the cursor
 			if err != nil {
-				g.Infow("Failed to process entry: %s", zap.Error(err))
+				plugin.Warnw("Failed to parse journal entry", zap.Error(err))
+				continue
 			}
-			// TODO checkpointing/offset tracking
+
+			err = plugin.Output.Process(entry)
+			if err != nil {
+				plugin.Infow("Failed to process entry: %s", zap.Error(err))
+			}
 		}
 	}()
 
 	return nil
+}
+
+func (plugin *JournaldInput) parseJournalEntry(line []byte) (*entry.Entry, string, error) {
+	var record map[string]interface{}
+	err := plugin.json.Unmarshal(line, &record)
+	if err != nil {
+		return nil, "", err
+	}
+
+	timestampInterface, ok := record["__REALTIME_TIMESTAMP"]
+	if !ok {
+		return nil, "", errors.New("journald record missing __REALTIME_TIMESTAMP field")
+	}
+
+	timestampString, ok := timestampInterface.(string)
+	if !ok {
+		return nil, "", errors.New("timestamp is not a string")
+	}
+
+	timestampInt, err := strconv.ParseInt(timestampString, 10, 64)
+	if err != nil {
+		return nil, "", fmt.Errorf("parse timestamp: %s", err)
+	}
+
+	delete(record, "__REALTIME_TIMESTAMP")
+
+	cursorInterface, ok := record["__CURSOR"]
+	if !ok {
+		return nil, "", errors.New("journald record missing __CURSOR field")
+	}
+
+	cursor, ok := cursorInterface.(string)
+	if !ok {
+		return nil, "", errors.New("cursor is not a string")
+	}
+
+	delete(record, "__CURSOR")
+
+	entry := &entry.Entry{
+		Timestamp: time.Unix(0, timestampInt*1000), // in microseconds
+		Record:    record,
+	}
+
+	return entry, cursor, nil
 }
 
 // Stop will stop generating logs.
