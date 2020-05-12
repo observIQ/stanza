9c8cca46e21ab317296ba52b9dbc78feb1b9fc24
Dan Jaglowski <dan.jaglowski@bluemedora.com>
2020-05-12 14:21:38 -0400

    Benchmark script parameterized and opens profiles

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 
+BPLOG_ROOT="$GOPATH/src/github.com/bluemedora/bplogagent"
+
+PROJECT="bindplane-agent-dev-0"
+ZONE="us-central1-a"
+INSTANCE="rhel6"
+
+if ! [ -x "$(command -v gcloud)" ]; then
+  echo 'Error: gcloud is not installed.' >&2
+  exit 1
+fi
+
+if ! [ -x "$(command -v dot)" ]; then
+  echo 'Error: dot is not installed.' >&2
+  exit 1
+fi
+
 echo "Building log agent"
-GOOS=linux go build -o /tmp/bplogagent ../cmd
+GOOS=linux go build -o /tmp/bplogagent $BPLOG_ROOT/cmd
 
-echo "Copying log agent"
-gcloud beta compute --project "***REMOVED***" ssh --zone "us-east1-b" 'instance-1' -- 'rm -rf /tmp/benchmark && mkdir /tmp/benchmark'
-gcloud beta compute --project "***REMOVED***" scp --zone "us-east1-b" /tmp/bplogagent 'instance-1:/tmp/benchmark'
+echo "Building benchmark tool"
+GOOS=linux go build -o /tmp/logbench github.com/awslabs/amazon-log-agent-benchmark-tool/cmd/logbench/
 
-echo "Downloading dependencies"
-gcloud beta compute --project "***REMOVED***" ssh --zone "us-east1-b" 'instance-1' -- \
-  'gsutil cp gs://bplogagent-logbench/LogBench /tmp/benchmark/LogBench && chmod +x /tmp/benchmark/LogBench'
-gcloud beta compute --project "***REMOVED***" ssh --zone "us-east1-b" 'instance-1' -- \
-  'gsutil cp gs://bplogagent-logbench/config.yaml /tmp/benchmark/config.yaml'
+echo "Setting up benchmark run on $PROJECT $ZONE $INSTANCE"
+gcloud beta compute --project "$PROJECT" ssh --zone "$ZONE" "$INSTANCE" -- 'rm -rf ~/benchmark && mkdir ~/benchmark'
+gcloud beta compute --project "$PROJECT" scp --zone "$ZONE" /tmp/bplogagent "$INSTANCE:~/benchmark"
+gcloud beta compute --project "$PROJECT" scp --zone "$ZONE" /tmp/logbench "$INSTANCE:~/benchmark/LogBench"
+gcloud beta compute --project "$PROJECT" scp --zone "$ZONE" $BPLOG_ROOT/scripts/benchmark/config.yaml "$INSTANCE:~/benchmark/config.yaml"
+gcloud beta compute --project "$PROJECT" ssh --zone "$ZONE" "$INSTANCE" -- 'chmod -R 777 ~/benchmark'
 
 echo "Running single-file benchmark"
-gcloud beta compute --project "***REMOVED***" ssh --zone "us-east1-b" 'instance-1' -- \
-  'set -m ;
-  cd /tmp/benchmark ;
-  /tmp/benchmark/LogBench -log stream.log -rate 100 -t 60s -r 30s -f 2s /tmp/benchmark/bplogagent --config /tmp/benchmark/config.yaml > output1 &
+gcloud beta compute --project "$PROJECT" ssh --zone "$ZONE" "$INSTANCE" -- \
+  'set -m
+  ~/benchmark/LogBench -log stream.log -rate 100 -t 60s -r 30s -f 2s ~/benchmark/bplogagent --config ~/benchmark/config.yaml > ~/benchmark/output1 2>&1 &
   sleep 10;
-  curl http://localhost:6060/debug/pprof/profile?seconds=30 > /tmp/benchmark/profile1 ;
+  curl http://localhost:6060/debug/pprof/profile?seconds=30 > ~/benchmark/profile1 ; 
   fg ; '
 
 echo "Running 20-file benchmark"
-gcloud beta compute --project "***REMOVED***" ssh --zone "us-east1-b" 'instance-1' -- \
-  'set -m ;
-  cd /tmp/benchmark ;
-  /tmp/benchmark/LogBench -log $(echo stream{1..20}.log | tr " " ,) -rate 100 -t 60s -r 30s -f 2s /tmp/benchmark/bplogagent --config /tmp/benchmark/config.yaml > output20 &
+gcloud beta compute --project "$PROJECT" ssh --zone "$ZONE" "$INSTANCE" -- \
+  'set -m
+  ~/benchmark/LogBench -log $(echo stream{1..20}.log | tr " " ,) -rate 100 -t 60s -r 30s -f 2s ~/benchmark/bplogagent --config ~/benchmark/config.yaml > ~/benchmark/output20 2>&1 &
   sleep 10;
-  curl http://localhost:6060/debug/pprof/profile?seconds=30 > /tmp/benchmark/profile20 ;
+  curl http://localhost:6060/debug/pprof/profile?seconds=30 > ~/benchmark/profile20 ; 
   fg ; '
 
-echo "Retrieving results"
-output_dir="/tmp/benchmark/$(date +%Y-%m-%d_%H-%M-%S)"
+output_dir="$BPLOG_ROOT/tmp/$(date +%Y-%m-%d_%H-%M-%S)"
 mkdir -p $output_dir
-gcloud beta compute --project "***REMOVED***" scp --zone "us-east1-b" 'instance-1:/tmp/benchmark/output*' "$output_dir"
-gcloud beta compute --project "***REMOVED***" scp --zone "us-east1-b" 'instance-1:/tmp/benchmark/profile*' "$output_dir"
-echo "Results are located in $output_dir"
+echo "Results will be located in $output_dir"
+
+echo "Retrieving results"
+gcloud beta compute --project "$PROJECT" scp --zone "$ZONE" "$INSTANCE:~/benchmark/output*" "$output_dir"
+gcloud beta compute --project "$PROJECT" scp --zone "$ZONE" "$INSTANCE:~/benchmark/profile*" "$output_dir"
+
+echo "Opening profiles"
+go tool pprof -http localhost:6001 $output_dir/profile1 &
+go tool pprof -http localhost:6020 $output_dir/profile20 &
