15dc78bf6a59728368b2e7554e94d8c0601f0b3c
Dan Jaglowski <dan.jaglowski@bluemedora.com>
2020-06-03 16:40:37 -0400

    Add benchmarking

---
+++ .circleci/scripts/startup-ttl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sleep 1800s
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
+
+# https://cloud.google.com/community/tutorials/create-a-self-deleting-virtual-machine
