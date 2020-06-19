fede87ef490adf133b8b71bdcadd1090afc69c6e
Camden Cheek <camden@ccheek.com>
2020-06-19 18:03:31 -0400

    Migrate journald

--- plugin/builtin/input/journald.go
+++ plugin/builtin/input/journald.go
@@ -59,7 +59,7 @@ func (c JournaldInputConfig) Build(buildContext plugin.BuildContext) (plugin.Plu
 
 	journaldInput := &JournaldInput{
 		InputPlugin: inputPlugin,
-		persist:     helper.NewScopedBBoltPersister(buildContext.Database, c.ID()),
+		persist:     helper.NewScopedDBPersister(buildContext.Database, c.ID()),
 		newCmd: func(ctx context.Context, cursor []byte) cmd {
 			if cursor != nil {
 				args = append(args, "--after-cursor", string(cursor))
