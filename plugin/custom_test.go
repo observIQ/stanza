393ceee2ad352e548f7927a99051937d02f18549
Camden Cheek <camden@ccheek.com>
2020-06-18 21:14:01 -0400

    Add custom plugin tests

--- plugin/custom_test.go
+++ plugin/custom_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+	"text/template"
 
 	"github.com/stretchr/testify/require"
 )
@@ -42,4 +43,65 @@ record:
 	require.NoError(t, err)
 
 	require.Equal(t, 2, len(customRegistry))
+}
+
+func TestCustomRegistryRender(t *testing.T) {
+	t.Run("ErrorTypeDoesNotExist", func(t *testing.T) {
+		reg := CustomRegistry{}
+		_, err := reg.Render("unknown", map[string]interface{}{})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "does not exist")
+	})
+
+	t.Run("ErrorExecFailure", func(t *testing.T) {
+		tmpl, err := template.New("customtype").Parse(`{{ .panicker }}`)
+		require.NoError(t, err)
+
+		reg := CustomRegistry{
+			"customtype": tmpl,
+		}
+		params := map[string]interface{}{
+			"panicker": func() {
+				panic("testpanic")
+			},
+		}
+		_, err = reg.Render("customtype", params)
+		require.Contains(t, err.Error(), "failed to render")
+	})
+}
+
+func TestCustomRegistryLoad(t *testing.T) {
+	t.Run("LoadAllBadGlob", func(t *testing.T) {
+		reg := CustomRegistry{}
+		err := reg.LoadAll("", `[]`)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "with glob pattern")
+	})
+
+	t.Run("AddDuplicate", func(t *testing.T) {
+		reg := CustomRegistry{}
+		var b Builder
+		Register("copy", b)
+		err := reg.Add("copy", "pipeline:\n")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "already exists")
+	})
+
+	t.Run("AddBadTemplate", func(t *testing.T) {
+		reg := CustomRegistry{}
+		err := reg.Add("new", "{{ nofunc }")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "as a custom template")
+	})
+
+	// t.Run("LoadAllWithFailures", func(t *testing.T) {
+	// 	tempDir := testutil.NewTempDir(t)
+	// 	pluginPath := filepath.Join(tempDir, "copy.yaml")
+	// 	err := ioutil.WriteFile(pluginPath, []byte("pipeline:\n"), 0755)
+	// 	require.NoError(t, err)
+
+	// 	reg := CustomRegistry{}
+	// 	err = reg.LoadAll(tempDir, "*.yaml")
+	// 	require.Error(t, err)
+	// })
 }
