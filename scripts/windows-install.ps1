ed4ec0a47d196362d74cdc4cbe10733d70817da7
Camden Cheek <camden@ccheek.com>
2020-07-14 15:12:22 -0400

    Stop service before downloading binary

--- scripts/windows-install.ps1
+++ scripts/windows-install.ps1
@@ -417,29 +417,6 @@ new-module -name LogAgentInstall -scriptblock {
     Remove-Indent
   }
 
-  # This will ensure that any previous installations are removed.
-  function Assert-CleanInstall {
-    Show-Header 'Ensuring Clean Installation'
-    Add-Indent
-    Remove-AgentService
-
-    if ( Test-Path "$script:agent_home" ) {
-      try {
-        Show-ColorText 'Previous installation detected at ' '' "$script:agent_home" DarkCyan '. Removing...'
-        Get-ChildItem "$script:agent_home" -Recurse | Remove-Item -Recurse -Force -ErrorAction Stop
-        Show-ColorText 'Previous installation files removed.'
-      }
-      catch {
-        Exit-Error $MyInvocation.ScriptLineNumber "Could not remove $script:agent_home: $($_.Exception.Message)" 'Please ensure you have permission to remove this directory and its files.'
-      }
-    }
-    else {
-      Show-ColorText 'Clean installation path detected. (' '' "$script:agent_home" DarkCyan ')'
-    }
-    Complete
-    Remove-Indent
-  }
-
   # This will remove the agent service.
   function Remove-AgentService {
     $service = Get-Service $SERVICE_NAME -ErrorAction SilentlyContinue
@@ -618,7 +595,7 @@ new-module -name LogAgentInstall -scriptblock {
         Test-Prerequisites
         Set-InstallVariables
         Set-Permissions
-        Assert-CleanInstall
+        Remove-AgentService
         Get-CarbonBinary
         New-AgentConfig
         New-AgentService
