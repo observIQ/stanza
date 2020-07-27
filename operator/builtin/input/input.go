9cae99017e27bd3fe283a72edc21d21b8e9abcf2
Joshua Williams <josh.williams@bluemedora.com>
2020-07-27 14:14:24 -0400

    Fixed imports for linux and darwin

--- operator/builtin/input/input.go
+++ operator/builtin/input/input.go
@@ -3,5 +3,4 @@ package input
 import (
 	// Load packages when importing input operators
 	_ "github.com/observiq/carbon/operator/builtin/input/file"
-	_ "github.com/observiq/carbon/operator/builtin/input/windows"
 )
