4e0b3530a78a8234e67a3e81d1072ef967db71ef
Camden Cheek <camden@ccheek.com>
2020-07-27 12:41:54 -0400

    Refactor custom scanner into its own object

--- operator/builtin/input/file/read_to_end.go
+++ operator/builtin/input/file/read_to_end.go
@@ -58,16 +58,7 @@ func ReadToEnd(
 		return fmt.Errorf("seek file: %s", err)
 	}
 
-	scanner := bufio.NewScanner(file)
-	buf := make([]byte, 0, 16384)
-	scanner.Buffer(buf, maxLogSize)
-	pos := startOffset
-	scanFunc := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
-		advance, token, err = splitFunc(data, atEOF)
-		pos += int64(advance)
-		return
-	}
-	scanner.Split(scanFunc)
+	scanner := NewPositionalScanner(file, maxLogSize, startOffset, splitFunc)
 
 	// Make a large, reusable buffer for transforming
 	decoder := encoding.NewDecoder()
@@ -113,24 +104,24 @@ func ReadToEnd(
 		}
 
 		emit(scanner.Bytes())
-		messenger.SetOffset(pos)
+		messenger.SetOffset(scanner.Pos())
 	}
 
 	// If we're not at the end of the file, and we haven't
 	// advanced since last cycle, read the rest of the file as an entry
-	if pos < stat.Size() && pos == startOffset && lastSeenFileSize == stat.Size() {
-		_, err := file.Seek(pos, 0)
+	if scanner.Pos() < stat.Size() && scanner.Pos() == startOffset && lastSeenFileSize == stat.Size() {
+		_, err := file.Seek(scanner.Pos(), 0)
 		if err != nil {
 			return errors.Wrap(err, "seeking for trailing entry")
 		}
 
-		msgBuf := make([]byte, stat.Size()-pos)
+		msgBuf := make([]byte, stat.Size()-scanner.Pos())
 		n, err := file.Read(msgBuf)
 		if err != nil {
 			return errors.Wrap(err, "reading trailing entry")
 		}
 		emit(msgBuf[:n])
-		messenger.SetOffset(pos + int64(n))
+		messenger.SetOffset(scanner.Pos() + int64(n))
 	}
 
 	return nil
