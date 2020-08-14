d35ab067ae6aa7981e586b0dd7f5b1e391bc34e8
Joshua Williams <josh.williams@bluemedora.com>
2020-08-14 14:09:49 -0400

    Added a host labeler to helpers (#92)
    * Added a host labeler to helpers
    
    * Implemented PR feedback

--- operator/builtin/transformer/host_metadata.go
+++ operator/builtin/transformer/host_metadata.go
@@ -2,8 +2,6 @@ package transformer
 
 import (
 	"context"
-	"net"
-	"os"
 
 	"github.com/observiq/carbon/entry"
 	"github.com/observiq/carbon/errors"
@@ -15,23 +13,18 @@ func init() {
 	operator.Register("host_metadata", func() operator.Builder { return NewHostMetadataConfig("") })
 }
 
-// Variables that are overridable for testing
-var hostname = os.Hostname
-
 // NewHostMetadataConfig returns a HostMetadataConfig with default values
 func NewHostMetadataConfig(operatorID string) *HostMetadataConfig {
 	return &HostMetadataConfig{
 		TransformerConfig: helper.NewTransformerConfig(operatorID, "host_decorator"),
-		IncludeHostname:   true,
-		IncludeIP:         true,
+		HostLabelerConfig: helper.NewHostLabelerConfig(),
 	}
 }
 
-//
+// HostMetadataConfig is the configuration of a host metadata operator
 type HostMetadataConfig struct {
 	helper.TransformerConfig `yaml:",inline"`
-	IncludeHostname          bool `json:"include_hostname,omitempty"     yaml:"include_hostname,omitempty"`
-	IncludeIP                bool `json:"include_ip,omitempty"     yaml:"include_ip,omitempty"`
+	helper.HostLabelerConfig `yaml:",inline"`
 }
 
 // Build will build an operator from the supplied configuration
@@ -41,76 +34,23 @@ func (c HostMetadataConfig) Build(context operator.BuildContext) (operator.Opera
 		return nil, errors.Wrap(err, "failed to build transformer")
 	}
 
-	op := &HostMetadata{
-		TransformerOperator: transformerOperator,
-		includeHostname:     c.IncludeHostname,
-		includeIP:           c.IncludeIP,
-	}
-
-	if c.IncludeHostname {
-		op.hostname, err = hostname()
-		if err != nil {
-			return nil, errors.Wrap(err, "get hostname")
-		}
-	}
-
-	if c.IncludeIP {
-		ip, err := getIP()
-		if err != nil {
-			return nil, errors.Wrap(err, "get ip address")
-		}
-		op.ip = ip
-	}
-
-	return op, nil
-}
-
-func getIP() (string, error) {
-	var ip string
-
-	ifaces, err := net.Interfaces()
+	hostLabeler, err := c.HostLabelerConfig.Build()
 	if err != nil {
-		return "", errors.Wrap(err, "list interfaces")
+		return nil, errors.Wrap(err, "failed to build host labeler")
 	}
 
-	for _, iface := range ifaces {
-		// Skip loopback interfaces
-		if iface.Flags&net.FlagLoopback != 0 {
-			continue
-		}
-
-		// Skip down interfaces
-		if iface.Flags&net.FlagUp == 0 {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			continue
-		}
-		if len(addrs) > 0 {
-			ip = addrs[0].String()
-		}
+	operator := &HostMetadata{
+		TransformerOperator: transformerOperator,
+		HostLabeler:         hostLabeler,
 	}
 
-	if len(ip) == 0 {
-		return "", errors.NewError(
-			"failed to find ip address",
-			"check that a non-loopback interface with an assigned IP address exists and is running",
-		)
-	}
-
-	return ip, nil
+	return operator, nil
 }
 
 // HostMetadata is an operator that can add host metadata to incoming entries
 type HostMetadata struct {
 	helper.TransformerOperator
-
-	hostname        string
-	ip              string
-	includeHostname bool
-	includeIP       bool
+	helper.HostLabeler
 }
 
 // Process will process an incoming entry using the metadata transform.
@@ -120,13 +60,6 @@ func (h *HostMetadata) Process(ctx context.Context, entry *entry.Entry) error {
 
 // Transform will transform an entry, adding the configured host metadata.
 func (h *HostMetadata) Transform(entry *entry.Entry) (*entry.Entry, error) {
-	if h.includeHostname {
-		entry.AddLabel("hostname", h.hostname)
-	}
-
-	if h.includeIP {
-		entry.AddLabel("ip", h.ip)
-	}
-
+	h.HostLabeler.Label(entry)
 	return entry, nil
 }
--- operator/builtin/transformer/host_metadata_test.go
+++ operator/builtin/transformer/host_metadata_test.go
@@ -2,7 +2,6 @@ package transformer
 
 import (
 	"context"
-	"os"
 	"sync"
 	"testing"
 
@@ -11,66 +10,6 @@ import (
 	"github.com/observiq/carbon/operator"
 	"github.com/stretchr/testify/require"
 )
-
-func testHostname() (string, error) {
-	return "test", nil
-}
-
-func TestHostMetadata(t *testing.T) {
-	cases := []struct {
-		name           string
-		modifyConfig   func(*HostMetadataConfig)
-		expectedLabels map[string]string
-		fakeHostname   func() (string, error)
-	}{
-		{
-			"Default",
-			func(cfg *HostMetadataConfig) {
-				cfg.IncludeIP = false
-			},
-			map[string]string{
-				"hostname": "test",
-			},
-			testHostname,
-		},
-		{
-			"NoHostname",
-			func(cfg *HostMetadataConfig) {
-				cfg.IncludeHostname = false
-				cfg.IncludeIP = false
-			},
-			nil,
-			testHostname,
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			hostname = tc.fakeHostname
-			defer func() { hostname = os.Hostname }()
-
-			cfg := NewHostMetadataConfig("test_id")
-			cfg.OutputIDs = []string{"fake"}
-			tc.modifyConfig(cfg)
-
-			op, err := cfg.Build(testutil.NewBuildContext(t))
-			require.NoError(t, err)
-
-			fake := testutil.NewFakeOutput(t)
-			err = op.SetOutputs([]operator.Operator{fake})
-			require.NoError(t, err)
-
-			e := entry.New()
-			op.Process(context.Background(), e)
-			select {
-			case r := <-fake.Received:
-				require.Equal(t, tc.expectedLabels, r.Labels)
-			default:
-				require.FailNow(t, "Expected entry")
-			}
-		})
-	}
-}
 
 type hostMetadataBenchmark struct {
 	name   string
