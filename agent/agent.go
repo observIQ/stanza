b47e6f2b9bb6b9873140cab38423c6a48ceadaa0
Camden Cheek <camden@ccheek.com>
2020-08-18 10:22:59 -0400

    Move pipeline build step into agent build step

--- agent/agent.go
+++ agent/agent.go
@@ -17,60 +17,25 @@ import (
 
 // LogAgent is an entity that handles log monitoring.
 type LogAgent struct {
-	Config    *Config
-	PluginDir string
-	Database  string
-	*zap.SugaredLogger
-
-	buildParams map[string]interface{}
-	database    operator.Database
-	pipeline    *pipeline.Pipeline
-	running     bool
+	database operator.Database
+	pipeline *pipeline.Pipeline
 
 	startOnce sync.Once
 	stopOnce  sync.Once
+
+	*zap.SugaredLogger
 }
 
 // Start will start the log monitoring process.
-func (a *LogAgent) Start() error {
-	var err error
+func (a *LogAgent) Start() (err error) {
 	a.startOnce.Do(func() {
-		database, err := OpenDatabase(a.Database)
-		if err != nil {
-			err = errors.Wrap(err, "open database")
-			return
-		}
-		a.database = database
-
-		registry, err := operator.NewPluginRegistry(a.PluginDir)
-		if err != nil {
-			a.Errorw("Failed to load plugin registry", zap.Any("error", err))
-		}
-
-		buildContext := operator.BuildContext{
-			PluginRegistry: registry,
-			Logger:         a.SugaredLogger,
-			Database:       a.database,
-			Parameters:     a.buildParams,
-		}
-
-		pipeline, err := a.Config.Pipeline.BuildPipeline(buildContext)
-		if err != nil {
-			err = errors.Wrap(err, "build pipeline")
-			return
-		}
-		a.pipeline = pipeline
-
 		err = a.pipeline.Start()
 		if err != nil {
-			err = errors.Wrap(err, "start pipeline")
 			return
 		}
-
 		a.Info("Agent started")
 	})
-
-	return err
+	return
 }
 
 // Stop will stop the log monitoring process.
@@ -78,9 +43,8 @@ func (a *LogAgent) Stop() {
 	a.stopOnce.Do(func() {
 		a.pipeline.Stop()
 		a.database.Close()
+		a.Info("Agent stopped")
 	})
-
-	a.Info("Agent stopped")
 }
 
 // OpenDatabase will open and create a database.
@@ -105,17 +69,32 @@ func OpenDatabase(file string) (operator.Database, error) {
 }
 
 // NewLogAgent creates a new carbon log agent.
-func NewLogAgent(cfg *Config, logger *zap.SugaredLogger, pluginDir, databaseFile string) *LogAgent {
-	return &LogAgent{
-		Config:        cfg,
-		SugaredLogger: logger,
-		PluginDir:     pluginDir,
-		Database:      databaseFile,
-		buildParams:   make(map[string]interface{}),
+func NewLogAgent(cfg *Config, logger *zap.SugaredLogger, pluginDir, databaseFile string, buildParams map[string]interface{}) (*LogAgent, error) {
+	database, err := OpenDatabase(databaseFile)
+	if err != nil {
+		return nil, errors.Wrap(err, "open database")
 	}
-}
 
-func (a *LogAgent) WithBuildParameter(key string, value interface{}) *LogAgent {
-	a.buildParams[key] = value
-	return a
+	registry, err := operator.NewPluginRegistry(pluginDir)
+	if err != nil {
+		return nil, errors.Wrap(err, "load plugin registry")
+	}
+
+	buildContext := operator.BuildContext{
+		PluginRegistry: registry,
+		Logger:         logger,
+		Database:       database,
+		Parameters:     buildParams,
+	}
+
+	pipeline, err := cfg.Pipeline.BuildPipeline(buildContext)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LogAgent{
+		pipeline:      pipeline,
+		database:      database,
+		SugaredLogger: logger,
+	}, nil
 }
