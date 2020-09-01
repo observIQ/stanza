0961983246416bb25283d035d8c99025cd63eb32
Camden Cheek <camden@ccheek.com>
2020-09-01 17:17:19 -0400

    Fix tests

--- operator/buffer/buffer_test.go
+++ operator/buffer/buffer_test.go
@@ -11,12 +11,12 @@ func TestBufferUnmarshalYAML(t *testing.T) {
 	cases := []struct {
 		name     string
 		input    []byte
-		expected BufferConfig
+		expected Config
 	}{
 		{
 			"SimpleMemory",
 			[]byte("type: memory\nmax_entries: 30\n"),
-			BufferConfig{
+			Config{
 				Type: "memory",
 				BufferBuilder: &MemoryBufferConfig{
 					MaxEntries: 30,
@@ -26,7 +26,7 @@ func TestBufferUnmarshalYAML(t *testing.T) {
 		{
 			"SimpleDisk",
 			[]byte("type: disk\nmax_size: 1234\npath: /var/log/testpath\n"),
-			BufferConfig{
+			Config{
 				Type: "disk",
 				BufferBuilder: &DiskBufferConfig{
 					MaxSize: 1234,
@@ -38,7 +38,7 @@ func TestBufferUnmarshalYAML(t *testing.T) {
 
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			var b BufferConfig
+			var b Config
 			err := yaml.Unmarshal(tc.input, &b)
 			require.NoError(t, err)
 			require.Equal(t, tc.expected, b)
