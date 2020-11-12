894579f81a74fc2aeb3eda3fecccb7499033d8c8
Camden Cheek <camden@ccheek.com>
2020-11-12 10:44:27 -0500

    Add full elastic_output test

--- operator/builtin/output/elastic/elastic_test.go
+++ operator/builtin/output/elastic/elastic_test.go
@@ -1,9 +1,17 @@
 package elastic
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/observiq/stanza/entry"
+	"github.com/observiq/stanza/testutil"
 	"github.com/stretchr/testify/require"
 )
 
@@ -79,4 +87,47 @@ func TestFindID(t *testing.T) {
 		require.NoError(t, err)
 		require.NotEmpty(t, idx)
 	})
+}
+
+func TestElastic(t *testing.T) {
+	received := make(chan []byte, 1)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		received <- body
+		w.WriteHeader(200)
+	}))
+	defer ts.Close()
+
+	cfg := NewElasticOutputConfig("test")
+	cfg.Addresses = []string{ts.URL}
+
+	ops, err := cfg.Build(testutil.NewBuildContext(t))
+	require.NoError(t, err)
+	op := ops[0]
+	e := entry.New()
+	e.Record = "test"
+
+	require.NoError(t, op.Start())
+	op.Process(context.Background(), e)
+	select {
+	case <-time.After(5 * time.Second):
+		require.FailNow(t, "Timed out waiting for request")
+	case body := <-received:
+		dec := json.NewDecoder(bytes.NewReader(body))
+
+		var meta map[string]map[string]interface{}
+		err := dec.Decode(&meta)
+		require.NoError(t, err)
+
+		var entry map[string]interface{}
+		err = dec.Decode(&entry)
+		require.NoError(t, err)
+
+		require.Equal(t, "default", meta["index"]["_index"])
+		require.Equal(t, float64(0), entry["severity"])
+		require.Equal(t, "test", entry["record"])
+	}
 }
