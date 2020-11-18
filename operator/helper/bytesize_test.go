f87e80d15eede70f8bd893e601e84e14f6f15c0c
Camden Cheek <camden@ccheek.com>
2020-11-18 16:04:44 -0500

    Deduplicate bytesize test cases

--- operator/helper/bytesize_test.go
+++ operator/helper/bytesize_test.go
@@ -9,40 +9,41 @@ import (
 	yaml "gopkg.in/yaml.v2"
 )
 
-func TestByteSizeUnmarshalJSON(t *testing.T) {
-	cases := []struct {
-		input       string
-		expected    ByteSize
-		expectError bool
-	}{
-		{`1`, 1, false},
-		{`1`, 1, false},
-		{`1`, 1, false},
-		{`3.3`, 3, false},
-		{`0`, 0, false},
-		{`10101010`, 10101010, false},
-		{`0.01`, 0, false},
-		{`"1"`, 1, false},
-		{`"1kb"`, 1000, false},
-		{`"1KB"`, 1000, false},
-		{`"1kib"`, 1024, false},
-		{`"1KiB"`, 1024, false},
-		{`"1mb"`, 1000 * 1000, false},
-		{`"1mib"`, 1024 * 1024, false},
-		{`"1gb"`, 1000 * 1000 * 1000, false},
-		{`"1gib"`, 1024 * 1024 * 1024, false},
-		{`"1tb"`, 1000 * 1000 * 1000 * 1000, false},
-		{`"1tib"`, 1024 * 1024 * 1024 * 1024, false},
-		{`"1pB"`, 1000 * 1000 * 1000 * 1000 * 1000, false},
-		{`"1pib"`, 1024 * 1024 * 1024 * 1024 * 1024, false},
-		{`1e3`, 1000, false},
-		{`"3ii3"`, 0, true},
-		{`3ii3`, 0, true},
-		{`--ii3`, 0, true},
-		{`{"test":"val"}`, 0, true},
-	}
+type testCase struct {
+	input       string
+	expected    ByteSize
+	expectError bool
+}
 
-	for i, tc := range cases {
+var sharedTestCases = []testCase{
+	{`1`, 1, false},
+	{`3.3`, 3, false},
+	{`0`, 0, false},
+	{`10101010`, 10101010, false},
+	{`0.01`, 0, false},
+	{`"1"`, 1, false},
+	{`"1kb"`, 1000, false},
+	{`"1KB"`, 1000, false},
+	{`"1kib"`, 1024, false},
+	{`"1KiB"`, 1024, false},
+	{`"1mb"`, 1000 * 1000, false},
+	{`"1mib"`, 1024 * 1024, false},
+	{`"1gb"`, 1000 * 1000 * 1000, false},
+	{`"1gib"`, 1024 * 1024 * 1024, false},
+	{`"1tb"`, 1000 * 1000 * 1000 * 1000, false},
+	{`"1tib"`, 1024 * 1024 * 1024 * 1024, false},
+	{`"1pB"`, 1000 * 1000 * 1000 * 1000 * 1000, false},
+	{`"1pib"`, 1024 * 1024 * 1024 * 1024 * 1024, false},
+	{`1e3`, 1000, false},
+	{`"3ii3"`, 0, true},
+	{`3ii3`, 0, true},
+	{`--ii3`, 0, true},
+	{`{"test":"val"}`, 0, true},
+	{`1e3`, 1000, false},
+}
+
+func TestByteSizeUnmarshalJSON(t *testing.T) {
+	for i, tc := range sharedTestCases {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			var bs ByteSize
 			err := json.Unmarshal([]byte(tc.input), &bs)
@@ -58,31 +59,7 @@ func TestByteSizeUnmarshalJSON(t *testing.T) {
 }
 
 func TestByteSizeUnmarshalYAML(t *testing.T) {
-	cases := []struct {
-		input       string
-		expected    ByteSize
-		expectError bool
-	}{
-		{`1`, 1, false},
-		{`1`, 1, false},
-		{`3.3`, 3, false},
-		{`0`, 0, false},
-		{`10101010`, 10101010, false},
-		{`0.01`, 0, false},
-		{`"1"`, 1, false},
-		{`"1kb"`, 1000, false},
-		{`"1KB"`, 1000, false},
-		{`"1kib"`, 1024, false},
-		{`"1KiB"`, 1024, false},
-		{`"1mb"`, 1000 * 1000, false},
-		{`"1mib"`, 1024 * 1024, false},
-		{`"1gb"`, 1000 * 1000 * 1000, false},
-		{`"1gib"`, 1024 * 1024 * 1024, false},
-		{`"1tb"`, 1000 * 1000 * 1000 * 1000, false},
-		{`"1tib"`, 1024 * 1024 * 1024 * 1024, false},
-		{`"1pB"`, 1000 * 1000 * 1000 * 1000 * 1000, false},
-		{`"1pib"`, 1024 * 1024 * 1024 * 1024 * 1024, false},
-		{`1`, 1, false},
+	additionalCases := []testCase{
 		{`1kb`, 1000, false},
 		{`1KB`, 1000, false},
 		{`1kib`, 1024, false},
@@ -95,14 +72,10 @@ func TestByteSizeUnmarshalYAML(t *testing.T) {
 		{`1tib`, 1024 * 1024 * 1024 * 1024, false},
 		{`1pB`, 1000 * 1000 * 1000 * 1000 * 1000, false},
 		{`1pib`, 1024 * 1024 * 1024 * 1024 * 1024, false},
-		{`"3ii3"`, 0, true},
-		{`3ii3`, 0, true},
-		{`--ii3`, 0, true},
-		{`{"test":"val"}`, 0, true},
 		{`test: val`, 0, true},
-		{`1e3`, 1000, false},
 	}
 
+	cases := append(sharedTestCases, additionalCases...)
 	for i, tc := range cases {
 		t.Run(strconv.Itoa(i), func(t *testing.T) {
 			var bs ByteSize
