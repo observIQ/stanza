614d37c5c63fcc94a13077ede282e6327f872090
Mrod1598 <miguelrodriguez1598@gmail.com>
2021-04-12 13:16:37 -0400

    Add move operator and the doc for it

---
+++ docs/operators/move.md
@@ -0,0 +1,283 @@
+## `move` operator
+
+The `move` operator moves (or renames) a field from one location to another.
+
+It's configured by passing 'to' and 'from' fields.
+
+### Configuration Fields
+
+| Field      | Default          | Description                                                                                                                                                                                                                              |
+| ---        | ---              | ---                                                                                                                                                                                                                                      |
+| `id`       | `move`    | A unique identifier for the operator                                                                                                                                                                                                     |
+| `output`   | Next in pipeline | The connected operator(s) that will receive all outbound entries                                                                                                                                                                         |
+| `from`      | required       | The [field](/docs/types/field.md)  to move the value out of.   
+| `to`      | required       | The [field](/docs/types/field.md)  to move the value into.
+| `on_error` | `send`           | The behavior of the operator if it encounters an error. See [on_error](/docs/types/on_error.md)                                                                                                                                          |
+| `if`       |                  | An [expression](/docs/types/expression.md) that, when set, will be evaluated to determine whether this operator should be used for the given entry. This allows you to do easy conditional parsing without branching logic with routers. |
+
+Example usage:
+
+Rename value
+```yaml
+- type: move
+    from: key1
+    to: key3
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "key1": "val1",
+    "key2": "val2"
+  }
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "key3": "val1",
+    "key2": "val2"
+  }
+}
+```
+
+</td>
+</tr>
+</table>
+<hr>
+
+Move a value from the record to resource
+
+```yaml
+- type: move
+    from: uuid
+    to: $resoruce.uuid
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "uuid": "091edc50-d91a-460d-83cd-089a62937738"
+  }
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { 
+    "uuid": "091edc50-d91a-460d-83cd-089a62937738"
+  },
+  "labels": { },  
+  "record": { }
+}
+```
+
+</td>
+</tr>
+</table>
+
+<hr>
+
+Move a value from the record to labels
+
+```yaml
+- type: move
+    from: ip
+    to: $labels.ip
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "ip": "8.8.8.8"
+  }
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { 
+    "ip": "8.8.8.8"
+  },  
+  "record": { }
+}
+```
+
+</td>
+</tr>
+</table>
+
+<hr>
+
+Replace the record with an individual value nested within the record
+```yaml
+- type: move
+    from: log
+    to: $record
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "log": "The log line"
+  }
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": "The log line"
+}
+```
+
+</td>
+</tr>
+</table>
+
+<hr>
+
+Remove a layer from the record
+```yaml
+- type: move
+    from: wrapper
+    to: $record
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "wrapper": {
+      "key1": "val1",
+      "key2": "val2",
+      "key3": "val3"
+    }
+  }
+}
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "key1": "val1",
+    "key2": "val2",
+    "key3": "val3"
+  }
+}
+```
+
+</td>
+</tr>
+</table>
+
+<hr>
+
+Merge a layer to the record
+```yaml
+- type: move
+    from: wrapper
+    to: $record
+```
+
+<table>
+<tr><td> Input Entry</td> <td> Output Entry </td></tr>
+<tr>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "wrapper": {
+      "key1": "val1",
+      "key2": "val2",
+      "key3": "val3"
+    },
+      "key4": "val1",
+      "key5": "val2",
+      "key6": "val3"
+  }
+}
+```
+
+</td>
+<td>
+
+```json
+{
+  "resource": { },
+  "labels": { },  
+  "record": {
+    "key1": "val1",
+    "key2": "val2",
+    "key3": "val3",
+    "key4": "val1",
+    "key5": "val2",
+    "key6": "val3"
+  }
+}
+```
+
+</td>
+</tr>
+</table>
+
---
+++ operator/builtin/transformer/move/move_test.go
@@ -0,0 +1,391 @@
+package move
+
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/observiq/stanza/entry"
+	"github.com/observiq/stanza/operator"
+	"github.com/observiq/stanza/testutil"
+)
+
+type processTestCase struct {
+	name      string
+	expectErr bool
+	op        *MoveOperatorConfig
+	input     func() *entry.Entry
+	output    func() *entry.Entry
+}
+
+func TestMoveProcess(t *testing.T) {
+	newTestEntry := func() *entry.Entry {
+		e := entry.New()
+		e.Timestamp = time.Unix(1586632809, 0)
+		e.Record = map[string]interface{}{
+			"key": "val",
+			"nested": map[string]interface{}{
+				"nestedkey": "nestedval",
+			},
+		}
+		return e
+	}
+
+	cases := []processTestCase{
+		{
+			"MoveRecordToRecord",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("key")
+				cfg.To = entry.NewRecordField("new")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"new": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"MoveRecordToLabel",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("key")
+				cfg.To = entry.NewLabelField("new")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				e.Labels = map[string]string{"new": "val"}
+				return e
+			},
+		},
+		{
+			"MoveLabelToRecord",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewLabelField("new")
+				cfg.To = entry.NewRecordField("new")
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Labels = map[string]string{"new": "val"}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"new": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				e.Labels = map[string]string{}
+				return e
+			},
+		},
+		{
+			"MoveLabelToResource",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewLabelField("new")
+				cfg.To = entry.NewResourceField("new")
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Labels = map[string]string{"new": "val"}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Resource = map[string]string{"new": "val"}
+				e.Labels = map[string]string{}
+				return e
+			},
+		},
+		{
+			"MoveResourceToLabel",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewResourceField("new")
+				cfg.To = entry.NewLabelField("new")
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Resource = map[string]string{"new": "val"}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Resource = map[string]string{}
+				e.Labels = map[string]string{"new": "val"}
+				return e
+			},
+		},
+		{
+			"MoveNest",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested")
+				cfg.To = entry.NewRecordField("NewNested")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"NewNested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"MoveFromNestedObj",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested", "nestedkey")
+				cfg.To = entry.NewRecordField("unnestedkey")
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key":         "val",
+					"nested":      map[string]interface{}{},
+					"unnestedkey": "nestedval",
+				}
+				return e
+			},
+		},
+		{
+			"MoveToNestedObj",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("newnestedkey")
+				cfg.To = entry.NewRecordField("nested", "newnestedkey")
+
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+					"newnestedkey": "nestedval",
+				}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey":    "nestedval",
+						"newnestedkey": "nestedval",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"MoveDoubleNestedObj",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested", "nested2")
+				cfg.To = entry.NewRecordField("nested2")
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+						"nested2": map[string]interface{}{
+							"nestedkey": "nestedval",
+						},
+					},
+				}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+					"nested2": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"MoveNestToResource",
+			true,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested")
+				cfg.To = entry.NewResourceField("NewNested")
+				return cfg
+			}(),
+			newTestEntry,
+			nil,
+		},
+		{
+			"MoveNestToLabel",
+			true,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested")
+				cfg.To = entry.NewLabelField("NewNested")
+
+				return cfg
+			}(),
+			newTestEntry,
+			nil,
+		},
+		{
+			"ReplaceRecordObj",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("wrapper")
+				cfg.To = entry.NewRecordField()
+				return cfg
+			}(),
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"wrapper": map[string]interface{}{
+						"key": "val",
+						"nested": map[string]interface{}{
+							"nestedkey": "nestedval",
+						},
+					},
+				}
+				return e
+			},
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key": "val",
+					"nested": map[string]interface{}{
+						"nestedkey": "nestedval",
+					},
+				}
+				return e
+			},
+		},
+		{
+			"ReplaceRecordString",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("key")
+				cfg.To = entry.NewRecordField()
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = "val"
+				return e
+			},
+		},
+		{
+			"MergeObjToRecord",
+			false,
+			func() *MoveOperatorConfig {
+				cfg := defaultCfg()
+				cfg.From = entry.NewRecordField("nested")
+				cfg.To = entry.NewRecordField()
+				return cfg
+			}(),
+			newTestEntry,
+			func() *entry.Entry {
+				e := newTestEntry()
+				e.Record = map[string]interface{}{
+					"key":       "val",
+					"nestedkey": "nestedval",
+				}
+				return e
+			},
+		},
+	}
+	for _, tc := range cases {
+		t.Run("BuildandProcess/"+tc.name, func(t *testing.T) {
+			cfg := tc.op
+			cfg.OutputIDs = []string{"fake"}
+			cfg.OnError = "drop"
+			ops, err := cfg.Build(testutil.NewBuildContext(t))
+			require.NoError(t, err)
+			op := ops[0]
+
+			move := op.(*MoveOperator)
+			fake := testutil.NewFakeOutput(t)
+			move.SetOutputs([]operator.Operator{fake})
+			val := tc.input()
+			err = move.Process(context.Background(), val)
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				fake.ExpectEntry(t, tc.output())
+			}
+		})
+	}
+}
+
+func defaultCfg() *MoveOperatorConfig {
+	return NewMoveOperatorConfig("move")
+}
