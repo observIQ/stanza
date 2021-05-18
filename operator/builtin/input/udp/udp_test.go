0ef6e6a3d779049ee300e507bb580c0ff7639e40
jsirianni <joe.sirianni@observiq.com>
2021-05-18 15:17:31 -0400

    add optional udp connection labels to record

--- operator/builtin/input/udp/udp_test.go
+++ operator/builtin/input/udp/udp_test.go
@@ -2,6 +2,7 @@ package udp
 
 import (
 	"net"
+	"strconv"
 	"testing"
 	"time"
 
@@ -61,11 +62,82 @@ func udpInputTest(input []byte, expected []string) func(t *testing.T) {
 	}
 }
 
+func udpInputLabelsTest(input []byte, expected []string) func(t *testing.T) {
+	return func(t *testing.T) {
+		cfg := NewUDPInputConfig("test_input")
+		cfg.ListenAddress = ":0"
+		cfg.AddLabels = true
+
+		ops, err := cfg.Build(testutil.NewBuildContext(t))
+		require.NoError(t, err)
+		op := ops[0]
+
+		mockOutput := testutil.Operator{}
+		udpInput, ok := op.(*UDPInput)
+		require.True(t, ok)
+
+		udpInput.InputOperator.OutputOperators = []operator.Operator{&mockOutput}
+
+		entryChan := make(chan *entry.Entry, 1)
+		mockOutput.On("Process", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+			entryChan <- args.Get(1).(*entry.Entry)
+		}).Return(nil)
+
+		err = udpInput.Start()
+		require.NoError(t, err)
+		defer udpInput.Stop()
+
+		conn, err := net.Dial("udp", udpInput.connection.LocalAddr().String())
+		require.NoError(t, err)
+		defer conn.Close()
+
+		_, err = conn.Write(input)
+		require.NoError(t, err)
+
+		for _, expectedRecord := range expected {
+			select {
+			case entry := <-entryChan:
+				expectedLabels := map[string]string{
+					"net.transport": "IP.UDP",
+				}
+				// LocalAddr for udpInput.connection is a server address
+				if addr, ok := udpInput.connection.LocalAddr().(*net.UDPAddr); ok {
+					expectedLabels["net.host.ip"] = addr.IP.String()
+					expectedLabels["net.host.port"] = strconv.FormatInt(int64(addr.Port), 10)
+				}
+				// LocalAddr for conn is a client (peer) address
+				if addr, ok := conn.LocalAddr().(*net.UDPAddr); ok {
+					expectedLabels["net.peer.ip"] = addr.IP.String()
+					expectedLabels["net.peer.port"] = strconv.FormatInt(int64(addr.Port), 10)
+				}
+				require.Equal(t, expectedRecord, entry.Record)
+				require.Equal(t, expectedLabels, entry.Labels)
+			case <-time.After(time.Second):
+				require.FailNow(t, "Timed out waiting for message to be written")
+			}
+		}
+
+		select {
+		case entry := <-entryChan:
+			require.FailNow(t, "Unexpected entry: %s", entry)
+		case <-time.After(100 * time.Millisecond):
+			return
+		}
+	}
+}
+
 func TestUDPInput(t *testing.T) {
 	t.Run("Simple", udpInputTest([]byte("message1"), []string{"message1"}))
 	t.Run("TrailingNewlines", udpInputTest([]byte("message1\n"), []string{"message1"}))
 	t.Run("TrailingCRNewlines", udpInputTest([]byte("message1\r\n"), []string{"message1"}))
 	t.Run("NewlineInMessage", udpInputTest([]byte("message1\nmessage2\n"), []string{"message1\nmessage2"}))
+}
+
+func TestUDPInputLabels(t *testing.T) {
+	t.Run("Simple", udpInputLabelsTest([]byte("message1"), []string{"message1"}))
+	t.Run("TrailingNewlines", udpInputLabelsTest([]byte("message1\n"), []string{"message1"}))
+	t.Run("TrailingCRNewlines", udpInputLabelsTest([]byte("message1\r\n"), []string{"message1"}))
+	t.Run("NewlineInMessage", udpInputLabelsTest([]byte("message1\nmessage2\n"), []string{"message1\nmessage2"}))
 }
 
 func BenchmarkUdpInput(b *testing.B) {
