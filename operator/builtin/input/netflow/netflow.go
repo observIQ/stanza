11c11932443a3f480150f16d8f89592c161999a3
jsirianni <joe.sirianni@bluemedora.com>
2021-06-14 14:01:36 -0400

    convert byte keys

--- operator/builtin/input/netflow/netflow.go
+++ operator/builtin/input/netflow/netflow.go
@@ -3,13 +3,17 @@ package netflow
 import (
 	"context"
 	"flag"
+	"fmt"
+	"net"
 	"runtime"
 	"sync"
 
 	flowmessage "github.com/cloudflare/goflow/v3/pb"
 	"github.com/cloudflare/goflow/v3/utils"
 	"github.com/fatih/structs"
+	"github.com/observiq/stanza/errors"
 	log "github.com/sirupsen/logrus"
+	"go.uber.org/zap"
 )
 
 var (
@@ -50,11 +54,28 @@ var (
 func (n NetflowInput) Publish(messages []*flowmessage.FlowMessage) {
 	go func() {
 		for _, msg := range messages {
-			// parse msg type into map[string]interface{} and
-			// respect json tags
 			structParser := structs.New(msg)
 			structParser.TagName = "json"
 			m := structParser.Map()
+
+			// https://github.com/cloudflare/goflow/blob/ddd88a7faa89bd9a8e75f0ceca17cbb443c14a8f/pb/flow.pb.go#L57
+			// IP address keys are []byte encoded
+			byteKeys := [...]string{
+				"SamplerAddress",
+				"SrcAddr",
+				"DstAddr",
+				"NextHop",
+				"SrcAddrEncap",
+				"DstAddrEncap",
+			}
+			var err error
+			for _, key := range byteKeys {
+				m, err = mapBytesToString(m, key)
+				if err != nil {
+					n.Errorf(fmt.Sprintf("error converting %s to string", key), zap.Error(err))
+				}
+
+			}
 
 			entry, err := n.NewEntry(m)
 			if err != nil {
@@ -134,4 +155,34 @@ func startGoFlow(transport utils.Transport, NFLEnable, NFLReuse bool, NFLAddr st
 		}()
 	}
 	wg.Wait()
+}
+
+// converts a key from []byte to string if it exists
+func mapBytesToString(m map[string]interface{}, key string) (map[string]interface{}, error) {
+	if val, ok := m[key]; ok {
+		delete(m, key)
+		switch x := val.(type) {
+		case []byte:
+			ip, err := bytesToIP(x)
+			if err != nil {
+				return nil, errors.Wrap(err, "error converting DstAddr to string")
+			}
+			m[key] = ip.String()
+			return m, nil
+		default:
+			return nil, fmt.Errorf("type %T cannot be parsed as an IP address", val)
+		}
+
+	}
+	return m, nil
+}
+
+func bytesToIP(b []byte) (net.IP, error) {
+	switch x := len(b); x {
+	case 4, 16:
+		var ip net.IP = b
+		return ip, nil
+	default:
+		return nil, fmt.Errorf("cannot convert byte slice to ip address, expected length of 4 or 16 got %d", x)
+	}
 }
