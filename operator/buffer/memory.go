2d6c0356c2009f1d85a3c892fa922dca67fc9a19
Corbin Phelps <cphelps8907@gmail.com>
2021-12-06 08:03:13 -0500

    Memory buffer rework (#497)
    * Implemented reworked memory buffer and started tests
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Working on memory buffer tests
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Fully tested memory buffer
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Removed memory_buffer*.old files
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Removed intentional race condition from memory buffer test so race flag wouldn't tirgger
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Fixed comment in memorybuffer build
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Renamed buffer closed error
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Fixed output operators. Most would fail on error equality to a wrapped error
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Fixed comment for memorybuffer
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- operator/buffer/memory.go
+++ operator/buffer/memory.go
@@ -2,6 +2,8 @@ package buffer
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
@@ -28,31 +30,112 @@ func NewMemoryBufferConfig() *MemoryBufferConfig {
 	}
 }
 
-// Build builds a MemoryBufferConfig into a Buffer, loading any entries that were previously unflushed
-// back into memory
+// Build builds a MemoryBufferConfig into a Buffer
 func (c MemoryBufferConfig) Build(operatorID string) (Buffer, error) {
-	return &MemoryBuffer{}, nil
+	return &MemoryBuffer{
+		operatorID:    operatorID,
+		buf:           make(chan *entry.Entry, c.MaxEntries),
+		maxChunkDelay: c.MaxChunkDelay.Raw(),
+		maxChunkSize:  c.MaxChunkSize,
+		closed:        false,
+	}, nil
 }
 
-// MemoryBuffer is a buffer that holds all entries in memory until Close() is called,
-// at which point it saves the entries into a database. It provides no guarantees about
-// lost entries if shut down uncleanly.
+// MemoryBuffer is a buffer that holds all entries in memory until Close() is called.
 type MemoryBuffer struct {
+	operatorID    string
+	buf           chan *entry.Entry
+	maxChunkDelay time.Duration
+	maxChunkSize  uint
+	closed        bool
+
+	// closeLock ensures that at the time of closing no Add or Read operations are occuring.
+	// Add and Read will use a closeLock.RLock to allow multiple concurrent operations.
+	// Close will use a closeLock.Lock to ensure all Add and Read operations are complete at the time of closing.
+	closeLock sync.RWMutex
 }
 
 // Add adds an entry onto the buffer.
 // Is a blocking call if the buffer is full
 func (m *MemoryBuffer) Add(ctx context.Context, e *entry.Entry) error {
-	return nil
+	m.closeLock.RLock()
+	defer m.closeLock.RUnlock()
+
+	// If buffer is closed don't allow this operation
+	if m.closed {
+		return ErrBufferClosed
+	}
+
+	// Insert into buffer or error if context finishes before we can
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("ctx error adding to buffer: %w", ctx.Err())
+	case m.buf <- e:
+		return nil
+	}
 }
 
 // Read reads from the buffer.
 // Read will block until the there are MaxChunkSize entries or we have block as long as MachChunkDelay.
 func (m *MemoryBuffer) Read(ctx context.Context) ([]*entry.Entry, error) {
-	return []*entry.Entry{}, nil
+	m.closeLock.RLock()
+	defer m.closeLock.RUnlock()
+
+	// If buffer is closed don't allow this operation
+	if m.closed {
+		return nil, ErrBufferClosed
+	}
+
+	entries := make([]*entry.Entry, 0, m.maxChunkSize)
+
+	// Time for max Chunk delay
+	// Using a timer here rather than a context with deadline as we don't want to confuse a context timeout with a maxChunkDelay timeout
+	timer := time.NewTimer(m.maxChunkDelay)
+	defer timer.Stop()
+
+LOOP:
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("ctx error during buffer read: %w", ctx.Err())
+		case <-timer.C:
+			// Timer has reached maxChunkDelay break and return
+			break LOOP
+		case e := <-m.buf:
+			entries = append(entries, e)
+
+			// If we've reached the maxChunkSize break and return
+			if len(entries) == int(m.maxChunkSize) {
+				break LOOP
+			}
+		}
+	}
+
+	return entries, nil
 }
 
 // Close runs cleanup code for buffer
 func (m *MemoryBuffer) Close() ([]*entry.Entry, error) {
-	return []*entry.Entry{}, nil
+	// Acquire lock so we can't close while Add or Read is occuring.
+	// It will also protect against multiple Close being called at once
+	m.closeLock.Lock()
+	defer m.closeLock.Unlock()
+
+	entries := make([]*entry.Entry, 0)
+
+	// Buffer already closed
+	if m.closed {
+		return entries, nil
+	}
+
+	// Mark as closed so any operations after this point won't execute
+	m.closed = true
+
+	// Close the buffer channel then drain it and return entries
+	close(m.buf)
+	for e := range m.buf {
+		entries = append(entries, e)
+	}
+
+	return entries, nil
 }
