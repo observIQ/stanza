fddd340fefe426750ef3dde92a2b1460142fd51c
jmwilliams89 <josh.williams@bluemedora.com>
2021-12-08 00:34:32 -0500

    Fixed client test

--- operator/builtin/output/googlecloud/client_test.go
+++ operator/builtin/output/googlecloud/client_test.go
@@ -6,10 +6,17 @@ import (
 
 	logging "cloud.google.com/go/logging/apiv2"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
 )
 
 func TestNewClient(t *testing.T) {
-	client, err := newClient(context.Background())
+	json := `{"type": "service_account"}`
+	credentials, err := google.CredentialsFromJSON(context.Background(), []byte(json), credentialScope)
+	require.NoError(t, err)
+
+	options := option.WithCredentials(credentials)
+	client, err := newClient(context.Background(), options)
 	require.NoError(t, err)
 
 	_, ok := client.(*logging.Client)
