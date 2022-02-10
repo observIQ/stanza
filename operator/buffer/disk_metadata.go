94a84eb62beb0b1dab37e1438d0ed9045984ac92
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-02-10 14:52:44 -0500

    Disk buffer rewrite (#539)
    * Ran go mod tidy
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Disk buffer rewrite
    
    * cleanup some code; bump up test coverage a bit.
    
    * Factor ErrEntryTooLarge into its own exported error
    
    * fix mutex copy in tests
    
    * use require.ErrorIs when asserting error type
    
    * tweak doc
    
    * Rewrite to use a circular buffer (ring buffer) -- in-progress
    
    * add old benchmarks in
    
    * Try using previous greedy strategy
    
    The old disk buffer uses an "unfair" strategy, where
    one reader will get all entries up to max chunk size before it times out.
    
    * Use 1 mebibyte for disk buffer benchmark
    
    * some performance changes
    
    * fix broken circular file
    
    * cleanup some todos and commented code
    
    * multierr in disk_metadata constructor
    
    * update path description
    
    * don't export things other than the buffer
    
    * rb -> cf, unexport some methods on circularFile struct
    
    * unexport another method on circularFile
    
    * Comment circular_file liberally
    
    * remove multierr, minor comments
    
    * unexport some more methods/structs
    
    * remove redundant seekedRead sets in discard
    
    * update docs; change marshalEntry to not take in a byte slice
    
    * remove ability to partial write
    
    * Capitalize struct methods used in other files
    
    * Make Start, End etc in circular file unexported
    
    * put cirularFile reads/writes into a for loop
    
    * refactor some conditionals with small helper functions
    
    Co-authored-by: Corbin Phelps <corbin.phelps@bluemedora.com>

---
+++ operator/buffer/disk_metadata.go
@@ -0,0 +1,132 @@
+package buffer
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"os"
+)
+
+type fileLike interface {
+	io.ReadWriteSeeker
+	io.Closer
+	Truncate(int64) error
+}
+
+// diskBufferMetadata holds metadata relating to the disk buffer,
+// that isn't the entry data itself.
+type diskBufferMetadata struct {
+	// Version is a number indicating the version of the disk buffer file.
+	// Currently, only 0 is valid.
+	Version uint8 `json:"version"`
+	// StartOffset is a number indicating the read offset in the file, such that File.Seek(StartOffset, io.SeekStart)
+	// should put the file cursor in the correct position for reading.
+	StartOffset int64 `json:"start"`
+	// EndOffset is a number indicating the write offset in the file, such that File.Seek(EndOffset, io.SeekStart)
+	// should put the file cursor in the correct position for writing.
+	EndOffset int64 `json:"end"`
+	// Full indicates whether the buffer is full or not
+	Full bool `json:"full"`
+	// Entries is the number of entries in the buffer
+	Entries int64 `json:"entries"`
+	// f is the internal file for reading and writing
+	f fileLike
+	// closed indicates whether the DiskBufferMetadata is closed
+	closed bool
+	// buf is the buffer used to write
+	buf *bytes.Buffer
+}
+
+func openDiskBufferMetadata(baseFilePath string, sync bool) (*diskBufferMetadata, error) {
+	fileFlags := os.O_CREATE | os.O_RDWR
+	if sync {
+		fileFlags |= os.O_SYNC
+	}
+
+	f, err := os.OpenFile(baseFilePath, fileFlags, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	bufBytes := make([]byte, 0, metadataBufferSize)
+	dbm := &diskBufferMetadata{
+		Version:     0,
+		StartOffset: 0,
+		EndOffset:   0,
+		f:           f,
+		buf:         bytes.NewBuffer(bufBytes),
+	}
+
+	fi, err := f.Stat()
+	if err != nil {
+		f.Close()
+		return nil, err
+	}
+
+	if fi.Size() > 0 {
+		err = dbm.ReadFromDisk()
+		if err != nil {
+			f.Close()
+			return nil, err
+		}
+	} else {
+		err = dbm.SyncToDisk()
+		if err != nil {
+			f.Close()
+			return nil, err
+		}
+	}
+
+	return dbm, nil
+}
+
+// metadataBufferSize is the initial size of the underlying buffer for disk metadata
+const metadataBufferSize = 1 << 10 // 1KiB
+
+// SyncToDisk syncs the diskBufferMetadata to the underlying file.
+func (d *diskBufferMetadata) SyncToDisk() error {
+	_, err := d.f.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	d.buf.Reset()
+	enc := json.NewEncoder(d.buf)
+	err = enc.Encode(d)
+	if err != nil {
+		return err
+	}
+
+	_, err = d.f.Write(d.buf.Bytes())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *diskBufferMetadata) ReadFromDisk() error {
+	_, err := d.f.Seek(0, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	enc := json.NewDecoder(d.f)
+	return enc.Decode(d)
+}
+
+func (d *diskBufferMetadata) Close() error {
+	if d.closed {
+		return nil
+	}
+
+	d.closed = true
+
+	err := d.SyncToDisk()
+	if err != nil {
+		d.f.Close()
+		return err
+	}
+
+	return d.f.Close()
+
+}
