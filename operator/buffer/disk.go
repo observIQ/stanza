b16f8f8d4b57a825c371bdaefbc1e727f0dceefd
Brandon Johnson <brandon.johnson@bluemedora.com>
2022-02-10 14:52:44 -0500

    Disk buffer rewrite (#539)
    * Ran go mod tidy
    
    Signed-off-by: Corbin Phelps <corbin.phelps@bluemedora.com>
    
    * Disk buffer rewrite
    
    * cleanup some code; bump up test coverage a bit.
    
    * Factor ErrEntryTooLarge into its own exported error
    
    * fix mutex copy in tests
    
    * use require.ErrorIs when asserting error type
    
    * tweak doc
    
    * Rewrite to use a circular buffer (ring buffer) -- in-progress
    
    * add old benchmarks in
    
    * Try using previous greedy strategy
    
    The old disk buffer uses an "unfair" strategy, where
    one reader will get all entries up to max chunk size before it times out.
    
    * Use 1 mebibyte for disk buffer benchmark
    
    * some performance changes
    
    * fix broken circular file
    
    * cleanup some todos and commented code
    
    * multierr in disk_metadata constructor
    
    * update path description
    
    * don't export things other than the buffer
    
    * rb -> cf, unexport some methods on circularFile struct
    
    * unexport another method on circularFile
    
    * Comment circular_file liberally
    
    * remove multierr, minor comments
    
    * unexport some more methods/structs
    
    * remove redundant seekedRead sets in discard
    
    * update docs; change marshalEntry to not take in a byte slice
    
    * remove ability to partial write
    
    * Capitalize struct methods used in other files
    
    * Make Start, End etc in circular file unexported
    
    * put cirularFile reads/writes into a for loop
    
    * refactor some conditionals with small helper functions
    
    Co-authored-by: Corbin Phelps <corbin.phelps@bluemedora.com>

--- operator/buffer/disk.go
+++ operator/buffer/disk.go
@@ -1,11 +1,18 @@
 package buffer
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
+	"golang.org/x/sync/semaphore"
 )
 
 var _ Buffer = (*DiskBuffer)(nil)
@@ -42,26 +49,186 @@ func NewDiskBufferConfig() *DiskBufferConfig {
 
 // Build creates a new Buffer from a DiskBufferConfig
 func (c DiskBufferConfig) Build() (Buffer, error) {
-	return &DiskBuffer{}, nil
+	if c.Path == "" {
+		return nil, os.ErrNotExist
+	}
+
+	bufferFilePath := filepath.Join(c.Path, "buffer")
+	metadataFilePath := filepath.Join(c.Path, "metadata.json")
+
+	metadata, err := openDiskBufferMetadata(metadataFilePath, c.Sync)
+	if err != nil {
+		return nil, err
+	}
+
+	cf, err := openCircularFile(bufferFilePath, c.Sync, int64(c.MaxSize), metadata)
+	if err != nil {
+		metadata.Close()
+		return nil, err
+	}
+
+	sem := semaphore.NewWeighted(int64(c.MaxSize))
+	acquired := sem.TryAcquire(cf.len())
+	if !acquired {
+		metadata.Close()
+		cf.Close()
+		return nil, errors.New("failed to acquire buffer length for semaphore")
+	}
+
+	return &DiskBuffer{
+		metadata:      metadata,
+		cf:            cf,
+		cfMux:         &sync.Mutex{},
+		writerSem:     sem,
+		readerSem:     newGreedyCountingSemaphore(metadata.Entries),
+		maxSize:       int64(c.MaxSize),
+		maxChunkDelay: c.MaxChunkDelay.Duration,
+		maxChunkSize:  c.MaxChunkSize,
+		closedMux:     &sync.RWMutex{},
+	}, nil
 }
 
-// TODO add comment
+// DiskBuffer is a buffer of entries that stores the entries to disk.
+// This buffer persists between application restarts.
 type DiskBuffer struct {
+	metadata *diskBufferMetadata
+	// f is the underlying byte buffer for the disk buffer
+	cf        *circularFile
+	cfMux     *sync.Mutex
+	writerSem *semaphore.Weighted
+	readerSem *greedyCountingSemaphore
+	// maxSize is the maximum number of entry bytes that can be written to the buffer file.
+	maxSize int64
+	// closed is a bool indicating if the buffer is closed
+	closed    bool
+	closedMux *sync.RWMutex
+
+	maxChunkDelay time.Duration
+	maxChunkSize  uint
 }
 
 // Add adds an entry onto the buffer.
-// Is a blocking call if the buffer is full
-func (m *DiskBuffer) Add(ctx context.Context, e *entry.Entry) error {
+// Will block if the buffer is full
+func (d *DiskBuffer) Add(ctx context.Context, e *entry.Entry) error {
+	d.closedMux.RLock()
+	defer d.closedMux.RUnlock()
+
+	if d.closed {
+		return ErrBufferClosed
+	}
+
+	bufBytes, err := marshalEntry(e)
+	if err != nil {
+		return err
+	}
+
+	// We cannot fit something into the file that exceeds the size of the file
+	if len(bufBytes) > int(d.maxSize) {
+		return ErrEntryTooLarge
+	}
+
+	err = d.writerSem.Acquire(ctx, int64(len(bufBytes)))
+	if err != nil {
+		return err
+	}
+
+	d.cfMux.Lock()
+	defer d.cfMux.Unlock()
+
+	_, err = d.cf.Write(bufBytes)
+	if err != nil {
+		return err
+	}
+
+	// Update metadata with current buffer state
+	d.metadata.Entries += 1
+	d.cf.SyncToMetadata(d.metadata)
+	err = d.metadata.SyncToDisk()
+	if err != nil {
+		return err
+	}
+
+	// Increment the counting semaphore to signal readers that an entry is available
+	d.readerSem.Increment()
+
 	return nil
 }
 
 // Read reads from the buffer.
-// Read will block until the there are MaxChunkSize entries or we have block as long as MachChunkDelay.
-func (m *DiskBuffer) Read(ctx context.Context) ([]*entry.Entry, error) {
-	return nil, nil
+// Read will block until the there are maxChunkSize entries or the duration maxChunkDelay has passed.
+func (d *DiskBuffer) Read(ctx context.Context) ([]*entry.Entry, error) {
+	d.closedMux.RLock()
+	defer d.closedMux.RUnlock()
+
+	if d.closed {
+		return nil, ErrBufferClosed
+	}
+
+	// The reader gains ownership of n entries here.
+	n := d.readerSem.AcquireAtMost(ctx, d.maxChunkDelay, int64(d.maxChunkSize))
+
+	if n == 0 {
+		return nil, ctx.Err()
+	}
+
+	entries := make([]*entry.Entry, 0, n)
+
+	d.cfMux.Lock()
+	defer d.cfMux.Unlock()
+
+	dec := json.NewDecoder(d.cf)
+
+	for i := int64(0); i < n; i++ {
+		var entry entry.Entry
+		err := dec.Decode(&entry)
+		if err != nil {
+			return entries, err
+		}
+
+		entries = append(entries, &entry)
+	}
+
+	decoderOffset := dec.InputOffset()
+	d.cf.Discard(decoderOffset)
+	d.writerSem.Release(decoderOffset)
+
+	// Update metadata with current buffer state
+	d.metadata.Entries -= n
+	d.cf.SyncToMetadata(d.metadata)
+	err := d.metadata.SyncToDisk()
+
+	return entries, err
 }
 
-// Close runs cleanup code for buffer
-func (m *DiskBuffer) Close() ([]*entry.Entry, error) {
-	return nil, nil
+// Close runs cleanup code for buffer.
+func (d *DiskBuffer) Close() ([]*entry.Entry, error) {
+	d.closedMux.Lock()
+	defer d.closedMux.Unlock()
+
+	if d.closed {
+		return nil, nil
+	}
+
+	d.closed = true
+
+	err := d.cf.Close()
+	if err != nil {
+		d.metadata.Close()
+		return nil, err
+	}
+
+	return nil, d.metadata.Close()
+}
+
+// marshalEntry marshals the given entry into a byte slice.
+// It returns a byte slice containing the marshalled entry.
+func marshalEntry(e *entry.Entry) ([]byte, error) {
+	buf := &bytes.Buffer{}
+	enc := json.NewEncoder(buf)
+	err := enc.Encode(e)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
