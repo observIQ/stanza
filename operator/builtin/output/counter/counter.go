9b160dc6f69477256cc39b9b845fe59f9db0981e
schmikei <keith.schmitt@bluemedora.com>
2022-02-21 17:01:26 -0500

    add counter_output operator

---
+++ operator/builtin/output/counter/counter.go
@@ -0,0 +1,139 @@
+package counter
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math"
+	"math/big"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/observiq/stanza/entry"
+	"github.com/observiq/stanza/operator"
+	"github.com/observiq/stanza/operator/helper"
+)
+
+type CounterOutputConfig struct {
+	helper.OutputConfig `yaml:",inline"`
+	Path                string          `json:"path" yaml:"path"`
+	Duration            helper.Duration `yaml:"duration,omitempty"`
+}
+
+var defaultCounterDuration = helper.NewDuration(1 * time.Minute)
+
+func init() {
+	operator.Register("counter_output", func() operator.Builder { return NewCounterOutputConfig("") })
+}
+
+func NewCounterOutputConfig(operatorID string) *CounterOutputConfig {
+	return &CounterOutputConfig{
+		OutputConfig: helper.NewOutputConfig(operatorID, "counter_output"),
+		Duration:     defaultCounterDuration,
+	}
+}
+
+func (c CounterOutputConfig) Build(bc operator.BuildContext) ([]operator.Operator, error) {
+	outputOperator, err := c.OutputConfig.Build(bc)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	counterOperator := &CounterOperator{
+		OutputOperator: outputOperator,
+		ctx:            ctx,
+		cancel:         cancel,
+		interval:       c.Duration.Duration,
+		path:           c.Path,
+		wg:             sync.WaitGroup{},
+	}
+
+	return []operator.Operator{counterOperator}, nil
+}
+
+type CounterOperator struct {
+	helper.OutputOperator
+	ctx      context.Context
+	interval time.Duration
+	start    time.Time
+	encoder  *json.Encoder
+	path     string
+	wg       sync.WaitGroup
+	cancel   context.CancelFunc
+
+	numEntries big.Int
+	size       int
+}
+
+func (co *CounterOperator) Process(ctx context.Context, e *entry.Entry) error {
+	co.numEntries.Add(&co.numEntries, big.NewInt(1))
+	return nil
+}
+
+func (co *CounterOperator) Start() error {
+	err := co.determineOutput()
+	if err != nil {
+		return err
+	}
+
+	co.start = time.Now()
+	co.wg.Add(1)
+	go co.startCounting()
+
+	return nil
+}
+
+// Stop tells the ForwardOutput to stop gracefully
+func (co *CounterOperator) Stop() error {
+	co.cancel()
+	co.wg.Wait()
+	return nil
+}
+
+func (co *CounterOperator) startCounting() {
+	defer co.wg.Done()
+
+	ticker := time.NewTicker(co.interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			err := co.logCount()
+			if err != nil {
+				return
+			}
+		case <-co.ctx.Done():
+			return
+		}
+	}
+}
+
+func (co *CounterOperator) logCount() error {
+	now := time.Now()
+	elapsedMinutes := math.Floor(now.Sub(co.start).Minutes())
+	entriesPerMinute := float64(co.numEntries.Int64()) / elapsedMinutes
+	msg := map[string]interface{}{
+		"entries":        co.numEntries,
+		"size":           co.size,
+		"elapsedMinutes": elapsedMinutes,
+		"entries/minute": entriesPerMinute,
+	}
+	return co.encoder.Encode(msg)
+}
+
+func (co *CounterOperator) determineOutput() error {
+	if co.path == "" {
+		co.encoder = json.NewEncoder(os.Stdout)
+		return nil
+	}
+
+	file, err := os.OpenFile(co.path, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0600)
+	if err != nil {
+		return fmt.Errorf("unable to write counter info to file located at %s: %w", co.path, err)
+	}
+	co.encoder = json.NewEncoder(file)
+	return nil
+}
