86b443ed7a21cc888eae8d9f539e78bcc08c34b4
jsirianni <joe.sirianni@bluemedora.com>
2022-02-21 20:12:16 -0500

    wix uninstall does not remove these files and directories

--- build/windows/test/uninstall.rb
+++ build/windows/test/uninstall.rb
@@ -1,28 +1,5 @@
 agent_home="C:/Program Files/stanza"
 
-[
-    "#{agent_home}/plugins"
-].each do |dir|
-    describe file(dir) do
-        it { should exist }
-        it { should be_directory }
-    end
-end
-
-[
-    "#{agent_home}/config.yaml",
-    "#{agent_home}/stanza.log",
-    "#{agent_home}/stanza.db",
-    "#{agent_home}/plugins/aerospike.yaml",
-    "#{agent_home}/plugins/microsoft_iis.yaml",
-    "#{agent_home}/plugins/zookeeper.yaml"
-].each do |file|
-    describe file(file) do
-        it { should exist }
-        it { should be_file }
-    end
-end
-
 describe service('stanza') do
     it { should_not be_enabled }
     it { should_not be_installed }
