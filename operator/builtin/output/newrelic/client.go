1a41ab20de49f37ff653161eb2c9c0f90b8ec725
armstrmi <mitchell.armstrong@bluemedora.com>
2022-02-25 11:49:40 -0500

    added newrelic close buffer

---
+++ operator/builtin/output/newrelic/client.go
@@ -0,0 +1,116 @@
+package newrelic
+
+import (
+	"bytes"
+	"compress/gzip"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/observiq/stanza/v2/version"
+	otelerrors "github.com/open-telemetry/opentelemetry-log-collection/errors"
+)
+
+// Client is an interface for sending a log payload to new relic
+type Client interface {
+	SendPayload(context.Context, LogPayload) error
+	TestConnection(context.Context) error
+}
+
+// client is the standard implementation of the Client interface
+type client struct {
+	endpoint   *url.URL
+	headers    http.Header
+	httpClient *http.Client
+}
+
+// NewClient creates a standard client for sending logs to new relic
+func NewClient(endpoint *url.URL, headers http.Header) Client {
+	return &client{
+		endpoint:   endpoint,
+		headers:    headers,
+		httpClient: &http.Client{},
+	}
+}
+
+// SendPayload creates an http request from a log payload and sends it to new relic
+func (c *client) SendPayload(ctx context.Context, payload LogPayload) error {
+	req, err := c.createRequest(ctx, payload)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if err := c.checkResponse(res); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TestConnection tests the connection to the new relic api
+func (c *client) TestConnection(ctx context.Context) error {
+	logs := make([]*LogMessage, 0, 0)
+	payload := LogPayload{{
+		Common: LogPayloadCommon{
+			Attributes: map[string]interface{}{
+				"plugin": map[string]interface{}{
+					"type":    "stanza",
+					"version": version.GetVersion(),
+				},
+			},
+		},
+		Logs: logs,
+	}}
+
+	err := c.SendPayload(ctx, payload)
+	if err != nil {
+		return fmt.Errorf("failed to send empty payload: %w", err)
+	}
+
+	return nil
+}
+
+// createRequest creates a new http.Request with the given context and log payload
+func (c *client) createRequest(ctx context.Context, payload LogPayload) (*http.Request, error) {
+	var buf bytes.Buffer
+	wr := gzip.NewWriter(&buf)
+	enc := json.NewEncoder(wr)
+	if err := enc.Encode(payload); err != nil {
+		return nil, otelerrors.Wrap(err, "encode payload")
+	}
+	if err := wr.Close(); err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "POST", c.endpoint.String(), &buf)
+	if err != nil {
+		return nil, err
+	}
+	req.Header = c.headers
+
+	return req, nil
+}
+
+// checkResponse checks a response from the new relic api
+func (c *client) checkResponse(res *http.Response) error {
+	defer func() {
+		_ = res.Body.Close()
+	}()
+
+	if !(res.StatusCode >= 200 && res.StatusCode < 300) {
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			return otelerrors.NewError("unexpected status code", "", "status", res.Status)
+		}
+		return otelerrors.NewError("unexpected status code", "", "status", res.Status, "body", string(body))
+	}
+	return nil
+}
--- operator/builtin/output/newrelic/newrelic_test.go
+++ operator/builtin/output/newrelic/newrelic_test.go
@@ -3,6 +3,7 @@ package newrelic
 import (
 	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net"
@@ -11,10 +12,13 @@ import (
 	"time"
 
 	"github.com/observiq/stanza/v2/operator/buffer"
+	"github.com/observiq/stanza/v2/operator/flusher"
 	"github.com/observiq/stanza/v2/testutil"
 	"github.com/open-telemetry/opentelemetry-log-collection/entry"
 	"github.com/open-telemetry/opentelemetry-log-collection/operator/helper"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
 func TestNewRelicConfigBuild(t *testing.T) {
@@ -43,13 +47,15 @@ func TestNewRelicConfigBuild(t *testing.T) {
 	})
 }
 
+type testCase struct {
+	name     string
+	cfgMod   func(*NewRelicOutputConfig)
+	input    []*entry.Entry
+	expected string
+}
+
 func TestNewRelicOutput(t *testing.T) {
-	cases := []struct {
-		name     string
-		cfgMod   func(*NewRelicOutputConfig)
-		input    []*entry.Entry
-		expected string
-	}{
+	cases := []testCase{
 		{
 			"Simple",
 			nil,
@@ -91,36 +97,19 @@ func TestNewRelicOutput(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			persiter := &testutil.MockPersister{}
 
-			ln := newListener()
-			addr, err := ln.start()
-			require.NoError(t, err)
+			op, ln := createTestOperator(t, tc)
 			defer ln.stop()
 
-			cfg := NewNewRelicOutputConfig("test")
-			cfg.BufferConfig = buffer.Config{
-				Builder: func() buffer.Builder {
-					cfg := buffer.NewMemoryBufferConfig()
-					cfg.MaxChunkDelay = helper.NewDuration(50 * time.Millisecond)
-					return cfg
-				}(),
-			}
-			cfg.BaseURI = fmt.Sprintf("http://%s/log/v1", addr)
-			cfg.APIKey = "testkey"
-			if tc.cfgMod != nil {
-				tc.cfgMod(cfg)
-			}
-
-			ops, err := cfg.Build(testutil.NewBuildContext(t))
-			require.NoError(t, err)
-			op := ops[0]
 			require.NoError(t, op.Start(persiter))
+			defer op.Stop()
+
 			for _, entry := range tc.input {
 				require.NoError(t, op.Process(context.Background(), entry))
 			}
-			defer op.Stop()
 
 			expectTestConnection(t, ln)
 			expectRequestBody(t, ln, tc.expected)
+
 		})
 	}
 
@@ -136,6 +125,38 @@ func TestNewRelicOutput(t *testing.T) {
 		err = op.Start(persiter)
 		require.Error(t, err)
 	})
+}
+
+func createTestOperator(t *testing.T, tc testCase) (*NewRelicOutput, *listener) {
+	ln := newListener()
+	addr, err := ln.start()
+	require.NoError(t, err)
+
+	config := NewNewRelicOutputConfig("test")
+	config.BufferConfig = buffer.Config{
+		Builder: func() buffer.Builder {
+			cfg := buffer.NewMemoryBufferConfig()
+			cfg.MaxChunkDelay = helper.NewDuration(50 * time.Millisecond)
+			return cfg
+		}(),
+	}
+
+	config.BaseURI = fmt.Sprintf("http://%s/log/v1", addr)
+	config.APIKey = "testkey"
+	if tc.cfgMod != nil {
+		tc.cfgMod(config)
+	}
+
+	buildContext := testutil.NewBuildContext(t)
+	operators, err := config.Build(buildContext)
+
+	require.NoError(t, err)
+	require.Len(t, operators, 1)
+
+	nro, ok := operators[0].(*NewRelicOutput)
+	require.True(t, ok)
+
+	return nro, ln
 }
 
 func expectTestConnection(t *testing.T, ln *listener) {
@@ -205,4 +226,194 @@ func handle(ch chan []byte) func(rw http.ResponseWriter, req *http.Request) {
 
 		ch <- body
 	}
+}
+
+func createTestOp(t *testing.T) (*NewRelicOutput, *MockClient) {
+	client := &MockClient{}
+
+	buffer, err := buffer.NewConfig().Build()
+	require.NoError(t, err)
+
+	flusher := flusher.NewConfig().Build(zap.NewNop().Sugar())
+	ctx, cancel := context.WithCancel(context.Background())
+
+	operator := NewRelicOutput{
+		messageField: entry.NewBodyField(),
+		buffer:       buffer,
+		client:       client,
+		flusher:      flusher,
+		ctx:          ctx,
+		cancel:       cancel,
+	}
+
+	return &operator, client
+}
+
+func TestCloseSuccessfulFlush(t *testing.T) {
+	operator, client := createTestOp(t)
+
+	entry := &entry.Entry{Body: "test message"}
+	operator.buffer.Add(context.Background(), entry)
+
+	sendChan := make(chan LogPayload, 1)
+	client.On("SendPayload", mock.Anything, mock.Anything).Run(func(args mock.Arguments) { sendChan <- args[1].(LogPayload) }).Return(nil)
+
+	err := operator.Stop()
+	require.NoError(t, err)
+
+	payloads := <-sendChan
+	require.Len(t, payloads, 1)
+
+	logs := payloads[0].Logs
+	require.Len(t, logs, 1)
+	require.Equal(t, "test message", logs[0].Message)
+}
+
+func TestCloseFailedFlush(t *testing.T) {
+	operator, client := createTestOp(t)
+
+	entry := &entry.Entry{Body: "test message"}
+	operator.buffer.Add(context.Background(), entry)
+
+	client.On("SendPayload", mock.Anything, mock.Anything).Return(errors.New("failed to flush"))
+
+	err := operator.Stop()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to flush")
+}
+func TestBufferCloseFailure(t *testing.T) {
+	buffer := &MockBuffer{}
+	operator, _ := createTestOp(t)
+	operator.buffer = buffer
+
+	buffer.On("Close").Return(nil, errors.New("failed to close buffer"))
+
+	err := operator.Stop()
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to close buffer")
+}
+
+func TestBufferReadFailure(t *testing.T) {
+	buffer := &MockBuffer{}
+	operator, client := createTestOp(t)
+	operator.buffer = buffer
+
+	buffer.On("Read", mock.Anything).Return(nil, errors.New("failure")).Once()
+	buffer.On("Read", mock.Anything).Return(nil, context.Canceled).Once()
+	buffer.On("Close").Return(nil, nil)
+
+	client.On("TestConnection", mock.Anything).Return(nil)
+
+	doneChan := make(chan struct{}, 1)
+	go func() {
+		defer close(doneChan)
+		operator.wg.Wait()
+	}()
+
+	timer := time.NewTimer(2 * time.Second)
+	defer timer.Stop()
+	select {
+	case <-doneChan:
+		//feedflusher exited successfully
+	case <-timer.C:
+		require.Fail(t, "test timed out")
+	}
+}
+
+// Buffer is an autogenerated mock type for the Buffer type
+type MockBuffer struct {
+	mock.Mock
+}
+
+// Add provides a mock function with given fields: _a0, _a1
+func (_m *MockBuffer) Add(_a0 context.Context, _a1 *entry.Entry) error {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, *entry.Entry) error); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// Close provides a mock function with given fields:
+func (_m *MockBuffer) Close() ([]*entry.Entry, error) {
+	ret := _m.Called()
+
+	var r0 []*entry.Entry
+	if rf, ok := ret.Get(0).(func() []*entry.Entry); ok {
+		r0 = rf()
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*entry.Entry)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// Read provides a mock function with given fields: _a0
+func (_m *MockBuffer) Read(_a0 context.Context) ([]*entry.Entry, error) {
+	ret := _m.Called(_a0)
+
+	var r0 []*entry.Entry
+	if rf, ok := ret.Get(0).(func(context.Context) []*entry.Entry); ok {
+		r0 = rf(_a0)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*entry.Entry)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// MockClient is an autogenerated mock type for the Client type
+type MockClient struct {
+	mock.Mock
+}
+
+// SendPayload provides a mock function with given fields: _a0, _a1
+func (_m *MockClient) SendPayload(_a0 context.Context, _a1 LogPayload) error {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, LogPayload) error); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// TestConnection provides a mock function with given fields: _a0
+func (_m *MockClient) TestConnection(_a0 context.Context) error {
+	ret := _m.Called(_a0)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context) error); ok {
+		r0 = rf(_a0)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
